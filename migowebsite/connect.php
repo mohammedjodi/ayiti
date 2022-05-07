476c22d9c20b4c1c1e58ce2bb6e018ce84bd16c9
akinyeleolubodun <akinyeleolubodun@gmail.com>
2022-05-07 17:09:00 +0100

    Removed daabase credentials

--- migowebsite/connect.php
+++ migowebsite/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-    $server = 'localhost';
-    $username = 'root';
-    $password = 'cynosure';
-    $db = 'AYITITEST';
+    $server = '';
+    $username = '';
+    $password = '';
+    $db = '';
 
     $blessing = mysqli_connect($server, $username, $password, $db);
     
